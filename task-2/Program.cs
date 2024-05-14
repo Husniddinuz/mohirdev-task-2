f90830be46c8c9676404a2e3d3700ee3ec32f758
Husniddin Tugizov <husniddindev@gmail.com>
2024-05-15 02:31:16 +0500

    2-task commit

---
+++ task-2/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Qiymatni kiriting: ");
+double qiymat = System.Convert.ToDouble(System.Console.ReadLine());
+
+System.Console.Write("Valyuta kursini kiriting (so'm): ");
+double kurs = System.Convert.ToDouble(System.Console.ReadLine());
+
+double summa = qiymat * kurs;
+
+System.Console.WriteLine($"Konvertatsiya qilingan summa: {summa} so'm");
